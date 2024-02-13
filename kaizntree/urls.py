2326d8ffa18972acc2c66d6c8837007ea9fb6610
nahianreza <91044737+nahianreza@users.noreply.github.com>
2024-02-13 02:38:59 -0500

    editing page-2

--- kaizntree/urls.py
+++ kaizntree/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
+from dashboard.views import ReadmeView
 
 
 schema_view = get_schema_view(
@@ -34,6 +34,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('', ReadmeView.as_view(), name='readme'),
     path('admin/', admin.site.urls),
     path('api/', include('dashboard.urls')),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
